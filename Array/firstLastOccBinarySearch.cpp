27e934d93d2fceae79f733e9a1cd2fa3a8a686cb
darshakkanani <darshakkanani444@gmail.com>
2024-03-19 11:16:20 +0530

    firstLast

--- Array/firstLastOccBinarySearch.cpp
+++ Array/firstLastOccBinarySearch.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 
@@ -75,6 +76,76 @@ int main()
     cout << "First occurance: " << first << " and "
          << "Last occurance: " << last << endl;
 }
+
+
+/*
+#include <iostream>
+using namespace std;
+int firstOcc(int arr[], int n, int key)
+{
+
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid])
+        { // Right me jao
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        { // left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int n, int key)
+{
+
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid])
+        { // Right me jao
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        { // left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 1};
+    int first = firstOcc(arr, 5, 1);
+    int last = lastOcc(arr, 5, 1);
+
+    cout << "First occurance: " << first << " and "
+         << "Last occurance: " << last << endl;
+}
+*/
 
 /*
 #include<iostream>
