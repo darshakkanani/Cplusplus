d797c1ab7b933793b4eebd817001749a113d6574
darshakkanani <darshakkanani444@gmail.com>
2024-03-19 09:07:57 +0530

    binarySearch

--- Array/binarySearch.cpp
+++ Array/binarySearch.cpp
@@ -1,26 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int search(int arr[],int key,int size)
+int search(int arr[], int key, int size)
 {
     int s = 0;
     int e = size - 1;
-    int mid = (s+e)/2;
+    int mid = s + (e - s) / 2;
 
-    while(s<=e)
+    while (s <= e)
     {
-        if(arr[mid]==key)
+        if (arr[mid] == key)
         {
             return mid;
         }
-        else if(arr[mid] < key)
+        else if (arr[mid] < key)
         {
             s = mid + 1;
         }
-        else if(arr[mid] > key)
+        else if (arr[mid] > key)
         {
             e = mid + 1;
         }
+        int mid = s + (e - s) / 2;
     }
     return -1;
 }
@@ -31,16 +32,16 @@ int main()
     cin >> size;
 
     int arr[100];
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
-    
-    int a ;
+
+    int a;
     cout << "Enter the key: ";
     cin >> a;
 
-    int ans = search(arr,a,size);
+    int ans = search(arr, a, size);
 
     cout << "Key faund at index no. " << ans << endl;
 }
