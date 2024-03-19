cbc469d7112be926a1b03df619578a116e44f73f
darshakkanani <darshakkanani444@gmail.com>
2024-03-19 09:03:56 +0530

    binarySearch

---
+++ Array/binarySearch.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int search(int arr[],int key,int size)
+{
+    int s = 0;
+    int e = size - 1;
+    int mid = (s+e)/2;
+
+    while(s<=e)
+    {
+        if(arr[mid]==key)
+        {
+            return mid;
+        }
+        else if(arr[mid] < key)
+        {
+            s = mid + 1;
+        }
+        else if(arr[mid] > key)
+        {
+            e = mid + 1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int size;
+    cout << "Enter trhe size of Array: ";
+    cin >> size;
+
+    int arr[100];
+    for(int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    int a ;
+    cout << "Enter the key: ";
+    cin >> a;
+
+    int ans = search(arr,a,size);
+
+    cout << "Key faund at index no. " << ans << endl;
+}
