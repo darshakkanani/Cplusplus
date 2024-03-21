996e7fb9175962c1fe6049314f3c6203d79a38bc
darshakkanani <darshakkanani444@gmail.com>
2024-03-21 06:41:37 +0530

    hi

---
+++ Array/keyFoundRotededArray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int keyR(int arr[],int size,int key)
+{
+    int s = 0;
+    int e = size - 1;
+    int mid = s + (e-s)/2;
+}
+// todo
+int main()
+{
+    int size;
+    cout << "Enter the size of an array: ";
+    cin >> size;
+
+    int arr[100];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "Enter the key: ";
+    cin >> key;
+}
