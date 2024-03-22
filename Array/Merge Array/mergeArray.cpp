507f35a6b131af53bc0603ecc5567b767f4ff11b
darshakkanani <darshakkanani444@gmail.com>
2024-03-22 13:17:47 +0530

    n

--- Array/Merge Array/mergeArray.cpp
+++ Array/Merge Array/mergeArray.cpp
@@ -32,11 +32,20 @@ void mergeArray(int arr1[],int n, int arr2[],int m,int arr3[])
         j++;
     }
 }
+
 void printArray(int arr[],int size)
 {
     for(int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
+    }
+}
+
+void scanArray(int arr[],int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
     }
 }
 int main()
@@ -48,26 +57,19 @@ int main()
 
     int arr1[100];
     cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr1[i];
-    }
+    scanArray(arr1,n);
+
     // int arr2[3] = {3,6,9};
     int m;
     cout << "Enter the size of an Array: ";
     cin >> m;
 
-    int f = m +n;
-
     int arr2[100];
     cout << "Enter the elements: ";
-    for (int i = 0; i < m; i++)
-    {
-        cin >> arr2[i];
-    }
-    
-    int arr3[200];
+    scanArray(arr2,m);
 
+    int arr3[200];
+    int f = m +n;
     mergeArray(arr1,n,arr2,m,arr3);
     cout << "After merge" << endl;
     printArray(arr3,f);
